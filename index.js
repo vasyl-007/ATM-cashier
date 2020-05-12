553162624cae2e750ead69a0454459fd9a8f66d2
Vasyl-007 <vasyl.manyo@gmail.com>
2020-05-12 17:14:10 +0300

    ATM-cashier start new app

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Hello Worls!");
+
+let iWantToGet = (amountRequired, limits) => {};
+
+let limits = { 1000: 5, 500: 2, 100: 5, 50: 100, 30: 6 };
+
+console.log(iWantToGet(230, limits));   // {30: 1, 100: 2}
+// console.log('limits', limits)
